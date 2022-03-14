6d2deb54bb838b17f417773d39ddf63780f3daa1
Nicolas Inostroza <ninostrozacordova2016@gmail.com>
2022-03-14 16:06:23 -0300

    Aplicacion de node base

---
+++ app.js
@@ -0,0 +1,41 @@
+const { inquirerMenu, pausa, leerInput } = require("./helper/inquirer");
+const Tareas = require("./models/tareas");
+
+
+require("colors");
+
+
+const main= async()=>{
+    
+
+    const tareas= new Tareas();
+     
+    let opt="";
+    do{
+        //Imprimir el menu
+        opt= await inquirerMenu();
+        
+        switch(opt){
+            case "1": 
+            //Crear la tarea
+            const descripcion= await leerInput("Descripcion de la tarea: ")
+            tareas.crearTarea(descripcion);
+            break;
+
+            case "2":
+                //Listar Tareas
+                console.log(tareas.listadoArr);
+
+            break;
+        }
+
+
+        await pausa();
+        
+        
+    } while(opt !=="0")
+
+    // pausa();
+}
+
+main();
